a76ac9f537d933e3464d0ab35d59fbef6e0f3b89
giovanni <giovanni.meciani@gmail.com>
2019-04-13 15:45:33 +0200

    Name are displayed as well. Added some parameter to the URL.

--- car_finno.py
+++ car_finno.py
@@ -4,14 +4,18 @@ import requests
 
 nrow      = '10000'
 priceFrom = '0'
-priceTo   = '500000'
+priceTo   = '50000'
+kmFrom    = '100000'
+kmTo      = '200000'
+yearFrom  = '1990'
+yearTo    = '2019'
+searchInTrondelag = '&location=20016' #leave empty to search everywhere
 # add body_type=4 for stationvagon
 url       = 'https://www.finn.no/car/used/search.html?'\
-            'dealer_segment=3&engine_fuel=0%2F1&location=20016&'\
-            'price_from='+priceFrom+'&price_to='+priceTo+'&'\
-            'registration_class=1&sort=4&rows='+nrow
+            'dealer_segment=3&engine_fuel=0%2F1'+searchInTrondelag+'&'\
+            '&mileage_from='+kmFrom+'&mileage_to='+kmTo+'&price_from='+priceFrom+'&price_to='+priceTo+'&'\
+            'registration_class=1&sort=4&year_from='+yearFrom+'&year_to='+yearTo+'&sales_form=1&rows='+nrow
 
-print(url)
 response = requests.get(url)
 
 cars = []
@@ -21,9 +25,11 @@ link  = ''
 year  = ''
 km    = ''
 kr    = ''
+name  = ''
 maxY  = 0
 maxKm = 0
 maxKr = 0
+tmpCounter = 0
 for line in response.iter_lines():
   if line:
     line = line.decode('utf-8')
@@ -39,23 +45,30 @@ for line in response.iter_lines():
         break
       link = 'http://finn.no/car/used/ad.html?finnkode='+t
       s = 1
-    elif s==1 and 'ads__unit__content__keys' in line:
-      s = 2
-    elif s==2:
+    elif (s==1 and 'data-search-resultitem>' in line) or (s==1 and tmpCounter>0):
+      if tmpCounter<1:
+        tmpCounter += 1
+      else:
+        name = line[12:len(line)]
+        s = 2
+        tmpCounter = 0
+    elif s==2 and 'ads__unit__content__keys' in line:
+      s = 3
+    elif s==3:
       tag = '<span>'
       year = int(line[line.find(tag)+len(tag):line.find('</span>')])
       if year > maxY:
         maxY = year
-      s = 3
-    elif s==3:
+      s = 4
+    elif s==4:
       tag = '<span>'
       km = line[line.find(tag)+len(tag):line.find(' km</span>')]
       km = ''.join(km.split())
       km = int(float(km))
       if km > maxKm and km!=12345678 and km!=99999999 and km!=0:
         maxKm = km
-      s = 4
-    elif s==4:
+      s = 5
+    elif s==5:
       tag = '<span>'
       kr = line[line.find(tag)+len(tag):line.find(' kr</span>')]
       kr = ''.join(kr.split())
@@ -64,7 +77,7 @@ for line in response.iter_lines():
         maxKr = kr      
 
       if km!=12345678 and km!=99999999 and km!=0:
-        cars.append([link, year, km, kr])
+        cars.append([link, name, year, km, kr])
 
       s = -1
 
@@ -74,16 +87,16 @@ best = ''
 worst = ''
 cars2 = []
 for i in cars:
-  year = i[1]
-  km = i[2]
-  kr = i[3]
+  year = i[2]
+  km = i[3]
+  kr = i[4]
 
   yi = maxY/year-1
   kmi = km/maxKm
   kri = kr/maxKr
   r = yi + kmi + kri
 
-  v = (i[0],i[1],i[2],i[3],r)
+  v = (i[0],i[2],i[3],i[4],r,i[1])
   cars2.append(v)
 
   if r > maxR:
